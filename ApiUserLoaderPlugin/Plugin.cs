9cdb5bed49a158ee2a2646942736cfbf175a3c4c
Rhein <rheinsakatoku@gmail.com>
2025-06-24 17:03:23 +0300

    fulltesttask

---
+++ ApiUserLoaderPlugin/Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.Interfaces;
+
+namespace ApiUserLoaderPlugin
+{
+    [Author(Name = "Rhein_Sakatoku")]
+    public class ApiUserLoaderPlugin : IPluggable
+    {
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> data)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            Console.WriteLine(">>>>> Плагин ApiUserLoaderPlugin загружен и вызван");
+
+            var users = new List<EmployeesDTO>();
+
+            try
+            {
+                using (var wc = new WebClient())
+                {
+                    wc.Headers.Add("User-Agent", "PhoneAppPlugin");
+                    var json = wc.DownloadString("https://dummyjson.com/users");
+                    var result = JsonConvert.DeserializeObject<UserApiResult>(json);
+
+                    foreach (var user in result.users)
+                        users.Add(new EmployeesDTO { Name = $"{user.firstName} {user.lastName}", Phone = user.phone });
+                }
+
+                Console.WriteLine($"ApiUserLoaderPlugin: загружено {users.Count} пользователей");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ApiUserLoaderPlugin: ошибка при загрузке данных - " + ex.Message);
+            }
+
+            var list = new List<DataTransferObject>(data);
+            list.AddRange(users);
+
+            return list;
+        }
+    }
+
+    public class UserApiResult
+    {
+        public List<User> users { get; set; }
+    }
+
+    public class User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+    }
+}
---
+++ EmployeesLoaderPlugin/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using PhoneApp.Domain;
+using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesLoaderPlugin
+{ 
+
+  [Author(Name = "Ivan Petrov")]
+  public class Plugin : IPluggable
+  {
+    private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            logger.Info("Loading employees");
+
+            // Загружаем новых сотрудников из ресурсов
+            var loadedEmployees = JsonConvert.DeserializeObject<List<EmployeesDTO>>(EmployeesLoaderPlugin.Properties.Resources.EmployeesJson);
+
+            foreach (var emp in loadedEmployees)
+            {
+                if (!string.IsNullOrWhiteSpace(emp.Phone) && emp.Phone != "-")
+                {
+                    emp.AddPhone(emp.Phone);
+                }
+            }
+
+            logger.Info($"Loaded {loadedEmployees.Count} employees");
+
+            // Объединяем с уже существующими
+            var all = args.Cast<EmployeesDTO>().ToList();
+            all.AddRange(loadedEmployees);
+
+            return all.Cast<DataTransferObject>();
+        }
+    }
+}
